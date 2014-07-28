3eb5bf18592c7b5e53cfe797efa1c9bdaf65da48
Kouki Saito <dan.addr.skd@gmail.com>
2014-07-28 14:24:46 +0900

    Change comp

--- src/confirm.py
+++ src/confirm.py
@@ -1,11 +1,13 @@
 
+
+
 def confirm(text):
   success = False
   while True:
     answer = input(text + " Y/n:")
-    if answer == "Y":
+    if answer in ["Y"]:
       success = True
       break
-    elif answer == "n" or answer == "N":
+    elif answer in ["n", "N"]:
       break
   return success
